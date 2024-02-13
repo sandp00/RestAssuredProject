d5f5398a5fe1a22760d45a56a48e8601c4b056ff
sandp00 <sandipsharmap@gmail.com>
2024-02-13 21:06:08 +0530

    added testcases

--- src/test/java/com/restTest/DemoTestClass.java
+++ src/test/java/com/restTest/DemoTestClass.java
@@ -1,6 +1,7 @@
 package com.restTest;
 
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
@@ -8,12 +9,21 @@ import static io.restassured.RestAssured.*;
 public class DemoTestClass {
 
     @Test
-    public void testAPI() {
+    public void testAPI1() {
         RestAssured.baseURI = "https://reqres.in";
         Response response=RestAssured.given()
                 .get("/api/users/2");
-        String data=response.getBody().toString();
+        String data=response.getBody().asString();
         System.out.println(data);
-
+        Assert.assertEquals(201,response.getStatusCode(),"Status code is not as expected");
+    }
+    @Test
+    public void testAPI2() {
+        RestAssured.baseURI = "https://reqres.in";
+        Response response=RestAssured.given()
+                .get("/api/users/2");
+        String data=response.getBody().asString();
+        System.out.println(data);
+        Assert.assertEquals(200,response.getStatusCode(),"Status code is not as expected");
     }
 }
